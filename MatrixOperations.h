2ddfec3028474ff016bbd2b113081ffce8d37630
sleo1234 <safta.leonard@yahoo.com>
2025-01-01 02:00:18 +0200

    adding newton raphson alorithm

--- MatrixOperations.h
+++ MatrixOperations.h
@@ -12,5 +12,5 @@ void printVector(vector<double> vec);
 vector<double> vecTimesMat(vector<vector<double>> mat,vector<double> vec);
 vector<double> vecDiff(vector<double> vec1, vector<double> vec2);
 double euclidean_norm(vector<double>);
-vector<vector<double>> computeJacobian(const function<vector<double>(const vector<double>&)>& equations,const vector<double>& vars, double stepSize = 1e-6)
+vector<vector<double>> computeJacobian(const function<vector<double>(const vector<double>&)>& equations,const vector<double>& vars, double stepSize = 1e-6);
 #endif // MATRIX_OPERATIONS_H
--- Solver.cpp
+++ Solver.cpp
@@ -1,33 +1,72 @@
-#include "MatrixOperations.h"
 #include <cmath>
 #include <iostream>
 #include <iomanip>
 #include <functional>
-
+#include "MatrixOperations.h"
 using namespace std;
 
 
-vector<double> newtonRaphson(const function<vector<double>(const vector<double>&)>& eq,
-const vector<double>& x0, double tol
-) {
+vector<double> newtonRaphson(const function<vector<double>(const vector<double>&)>& eq, double tol
+,vector<double> x0) {
 
 int numVars = x0.size();
 int maxIter = 100;
 int iter = 0;
+int flag=0;
 
 
 vector<double> sol(numVars);
 vector<double> F(numVars);
+vector<double> diff(numVars);
 
- for (int i=0; i<numVars; i++){
+vector<vector<double>> x(maxIter,vector<double>(numVars));
+
+x[0] =x0;
+
+ for (int i=1; i < maxIter; i++){
+//printMatrix(computeJacobian(eq,x[i-1]));
 
 
+x[i]=vecDiff(x[i-1] , vecTimesMat(invertMatrix(computeJacobian(eq,x[i-1])),eq(x[i-1]))); 
 
+cout<<"Solution "<<endl;
 
+printVector(x[i]);
+cout<<" at "<<i<<"iteration ";
+
+if (euclidean_norm(vecDiff(x[i],x[i-1]))<= tol){
+ sol = x[i];
+ flag = 1;
+
+}
+
+ if (flag>0) break;
 
  }
 
-
+printVector(sol);
 return sol;
 
+}
+
+vector<double> sys(const vector<double>& vars){
+ 
+ double x1 = vars[0];
+ double x2 = vars[1];
+// double x3 = vars[2];
+ 
+ return {x1+2*x2-5,  
+        x1+x2+2};
+
+ 
+}
+
+
+int main(){
+
+vector<double> x0 = {1.0,0.5};
+double tol=1e-4;
+
+newtonRaphson(sys,tol,x0);
+return 0;
 }
